c379460ff11e78a5107a620c264dea308f037e82
amanqs <116822442+amanqs@users.noreply.github.com>
2022-10-27 22:06:53 +0700

    Update funny.py

--- EmikoRobot/modules/funny.py
+++ EmikoRobot/modules/funny.py
@@ -13,6 +13,6 @@ from EmikoRobot import telethn as tbot
 @register(pattern=("/memek"))
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), RAIMU KOYOK KONTOL.** \n\n"
-  TEXT = f"**NGOPO LENG?,RA TERIMO??
+  TEXT = f"**NGOPO LENG?,RA TERIMO??"
   BUTTON = [[Button.url("Help", "https://t.me/amanqss?start=help"), Button.url("KONTOL", "https://xnxx.com)]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
